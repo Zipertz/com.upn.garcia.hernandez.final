805685a03a87b46236b0aad6c53a83e7bad2da0f
Jhordan Garcia <jhordantm@gmail.com>
2023-07-06 12:02:50 -0500

    octavo

--- app/src/main/java/com/example/comupngarciahernandezfinal/DetalleCartaActivity.java
+++ app/src/main/java/com/example/comupngarciahernandezfinal/DetalleCartaActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 
 import com.example.comupngarciahernandezfinal.db.DbYuGiHo;
 import com.example.comupngarciahernandezfinal.modelos.Carta;
+import com.squareup.picasso.Picasso;
 
 public class DetalleCartaActivity extends AppCompatActivity {
     private EditText tvNombreCarta;
@@ -18,6 +19,7 @@ public class DetalleCartaActivity extends AppCompatActivity {
     private EditText tvDefensa;
     private EditText tvLatitud;
     private EditText tvLongitud;
+    private EditText tvImagenCarta;
     private ImageView imageMonstruo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +31,31 @@ public class DetalleCartaActivity extends AppCompatActivity {
         tvDefensa = findViewById(R.id.tvDefensa);
         tvLatitud = findViewById(R.id.tvLatitud);
         tvLongitud = findViewById(R.id.tvLongitud);
+        tvImagenCarta = findViewById(R.id.tvImagenCarta);
         imageMonstruo = findViewById(R.id.imageMonstruo);
         // Obtener los datos de la carta enviados desde la actividad anterior
         Intent intent = getIntent();
         if (intent != null) {
             int cartaId = intent.getIntExtra("cartaId", 0);
 
+
             // Obtener los detalles de la carta desde la base de datos o cualquier otra fuente de datos
             Carta carta = obtenerDetalleCarta(cartaId);
             if (carta != null) {
+
+                String imagenPath = carta.getImagen();
+                Bitmap bitmap = BitmapFactory.decodeFile(imagenPath);
+                imageMonstruo.setImageBitmap(bitmap);
                 // Mostrar los detalles de la carta en los TextView correspondientes
                 tvNombreCarta.setText(carta.getMounstro());
+                tvImagenCarta.setText("imagen: " + carta.getImagen());
                 tvAtaque.setText("Ataque: " + carta.getAtaque());
                 tvDefensa.setText("Defensa: " + carta.getDefensa());
                 tvLatitud.setText("Latitud: " + carta.getLatitud());
                 tvLongitud.setText("longitud: " + carta.getLongitud());
 
-                tvLongitud.setText("longitud: " + carta.getLongitud());
 
-                // Cargar la imagen desde el archivo y establecerla en el ImageView
-                Bitmap imagenBitmap = BitmapFactory.decodeFile(carta.getImagen());
-                if (imagenBitmap != null) {
-                    imageMonstruo.setImageBitmap(imagenBitmap);
-                }
+
             }
         }
     }
